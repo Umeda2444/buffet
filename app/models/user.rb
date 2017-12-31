5c2b1281b171dad9dc44d6028ffcc2b2b7e1524f
梅田佳孝 <yumeda03@hotmail.com>
2017-12-31 12:05:01 +0900

    12/30

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews
   has_many :restaurants, through: :reviews
+
+  def mood_average
+    reviews.average(:mood).round(1) if reviews.present?
+  end
+
+  def food_average
+    reviews.average(:food).round(1) if reviews.present?
+  end
+
+  def service_average
+    reviews.average(:service).round(1) if reviews.present?
+  end
+
 end
