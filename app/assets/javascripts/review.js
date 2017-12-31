d1134e254b2b1f4b7996bc055764cb6db5c8a950
梅田佳孝 <yumeda03@hotmail.com>
2018-01-01 08:52:13 +0900

    12/31

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,13 +1,17 @@
-window.addEventListener("load", function() {
+document.addEventListener("turbolinks:load", function() {
+
 // 雰囲気、料理、サービスの総合を計算して返す
 
-$("#renew").on("click", function() {
+  $("#renew").on("click", function() {
     var mood_ary = ["temperature", "music", "design", "creanliness"];
     var food_ary = ["umami", "amami", "sanmi", "siomi", "nigami"];
     var service_ary = ["speed", "care", "value", "custom"];
-    var mood_ave = 5;
-    var food_ave = 5;
-    var service_ave = 5;
+    var mood_ave = 50;
+    var food_ave = 50;
+    var service_ave = 50;
+    var mood_ave_face = 5;
+    var food_ave_face = 5;
+    var service_ave_face = 5;
 
     function review_ave(ary) {
       var num = 0;
@@ -17,18 +21,28 @@ $("#renew").on("click", function() {
         num = $('input#review_' + ary[i]).val();
         sum += Number(num);
       }
-        ave = sum / ary.length;
-        return Math.round(ave);
+        ave = sum/ ary.length;
+        return ave;
     }
 
     mood_ave = review_ave(mood_ary);
     food_ave = review_ave(food_ary);
     service_ave = review_ave(service_ary);
 
-//イベントリスナ==============================================================//
+    // mood_ave_face = mood_ave / 10;
+    // food_ave_face = food_ave / 10;
+    // service_ave_face = service_ave / 10;
+
     document.getElementById('review_mood').value=mood_ave;
     document.getElementById('review_food').value=food_ave;
     document.getElementById('review_service').value=service_ave;
+
+    // document.getElementById('review_mood_face').innerHTML=mood_ave_face;
+    // document.getElementById('review_food_face').innerHTML=food_ave_face;
+    // document.getElementById('review_service_face').innerHTML=service_ave_face;
+
+
+
   });
 });
 
